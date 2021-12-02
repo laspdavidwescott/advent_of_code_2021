abfe90b927335b6092fc85a15c299a09c8d33a02
David Wescott <david.wescott@lasp.colorado.edu>
2021-12-02 12:06:58 -0700

    2021 day 2 code completed.
    Added readme.md for each day.
    
    Added example input files.

---
+++ day_2/pilot_submarine_by_aiming.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+
+
+def main():
+    """ Read in the piloting instructions provided by the given file. Report
+        the submarine's final position.
+    """
+    ###########################################################################
+    # Command line argument parser
+    ###########################################################################
+
+    description = "Read in the piloting instructions provided by the given file. Report the submarine's final position."
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("file", help="Text file with depth values. One value per line.")
+
+    args = parser.parse_args()
+
+    ###########################################################################
+    # Read in directions
+    ###########################################################################
+
+    try:
+        with open(args.file, 'r') as FILE:
+            directions = FILE.readlines()
+    except Exception:
+        raise
+
+    ###########################################################################
+    # Calculate submarine location
+    ###########################################################################
+
+    # The depth and horizontal positions
+    depth_position = 0
+    horizontal_position = 0
+    aim = 0
+
+    # Analyze each direction
+    for direction in directions:
+        # Split the direction into command and value
+        command, value = re.split(r"\s+", direction.strip())
+
+        # Sanitize the value and command
+        value = int(value)
+        command = command.lower()
+
+        # Horizontal forward
+        if command == "forward":
+            horizontal_position += value
+            depth_position += aim * value
+        # Depth down
+        elif command == "down":
+            aim += value
+        # Depth up
+        elif command == "up":
+            aim -= value
+        # Unknown command
+        else:
+            print("ERROR: Unrecognized command: {}".format(command))
+
+    ###########################################################################
+    # Report
+    ###########################################################################
+
+    print("Depth Position:      {}".format(depth_position))
+    print("Horizontal Position: {}".format(horizontal_position))
+    print("Aim:                 {}".format(aim))
+    print("Multiplied Together: {}".format(depth_position * horizontal_position))
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
